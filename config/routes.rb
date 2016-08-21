3126a5cc3c9d5efef5505555892b264aa8303bf2
nelson <nyalsyon@gmail.com>
2016-08-21 12:01:53 +0545

    created user scaffold and migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
   root 'welcome#index'
   get 'welcome/index', to: 'welcome#index'
-  
+
 
 
 end
