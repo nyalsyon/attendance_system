399148f20f31901fb7c42c2fb19608dabdb2f521
nelson <nyalsyon@gmail.com>
2016-08-21 16:00:52 +0545

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    validates :name,
+    presence: true
+    validates :username,
+    presence: true, uniqueness: true, length: {minimum:4, maximum:10}
+    validates :password,
+    presence:true,length: {minimum:4, maximum:20}
+    validates :email
+    presence: true,
 end
